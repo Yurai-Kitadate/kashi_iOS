69a649a5663916341cffe031e1fc51fced8d7748
Yurai-Kitadate <yuurai18@gmail.com>
2022-08-18 12:15:48 +0900

    Initial Commit

---
+++ Shared/kashi_kashiApp.swift
@@ -0,0 +1,17 @@
+//
+//  kashi_kashiApp.swift
+//  Shared
+//
+//  Created by 北舘友頼 on 2022/08/18.
+//
+
+import SwiftUI
+
+@main
+struct kashi_kashiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
